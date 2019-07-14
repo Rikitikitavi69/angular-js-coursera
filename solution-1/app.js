cbdcae5b31f1414a0c82a06cec9edddd3f766605
Sergey Lazarenko <rikitikitavi69@gmail.com>
2019-07-14 21:09:51 +0300

    added solution 1

---
+++ solution-1/app.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController ($scope) {
+        $scope.checkDishes = function  () {
+            var num = countDishes($scope.dishes);
+            $scope.message = getMessage(num);
+        }
+    }
+    function countDishes(dishes) {
+        if (dishes === undefined) {
+            return 0
+        }
+        return dishes.split(',').length;
+    }   
+
+    function getMessage(num) {
+        if (num <= 3) {
+            return "Enjoy!";
+        } else {
+            return "Too much!";
+        }
+    }
+
+})();
