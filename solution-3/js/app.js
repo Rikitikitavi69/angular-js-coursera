871ba796ecd983325fb5b7ff896b9cd1d800d219
Sergey Lazarenko <rikitikitavi69@gmail.com>
2019-09-22 13:14:26 +0300

    forgot app

--- solution-3/js/app.js
+++ solution-3/js/app.js
@@ -7,52 +7,57 @@
     .directive('foundItems', foundItemsDirective)
     .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
+
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
-        var narrowItDown = this;
-
-        narrowItDown.search = function(searchTerm) {
-            if (searchTerm) {
-              var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-        
-              promise.then(function (response) {
-                narrowItDown.found = response;
-              })
-              .catch(function (error) {
-                narrowItDown.found = [];
-              });
-            } else {
-              narrowItDown.found = [];
-            }
-          };
-        
-          narrowItDown.removeItem = function(index) {
-            narrowItDown.found.splice(index, 1);
-          };
+      var menuSearch = this;
+    
+      menuSearch.search = function(searchTerm) {
+        if (searchTerm) {
+          var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+    
+          promise.then(function (response) {
+            menuSearch.found = response;
+          })
+          .catch(function (error) {
+            menuSearch.found = [];
+          });
+        } else {
+          menuSearch.found = [];
+        }
+      };
+    
+      menuSearch.removeItem = function(index) {
+        menuSearch.found.splice(index, 1);
+      };
     }
 
-    MenuSearchService.$inject = ['$http', '$q', 'ApiBasePath'];
-    function MenuSearchService($http, $q, ApiBasePath) {
-        var service = this;
-        service.getMatchedMenuItems = function (searchTerm) {
-            var response = $http({
-                method: "GET",
-                url: (ApiBasePath + "/menu_items.json")
-          });
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+    service.getMatchedMenuItems = function (searchTerm) {
+        var response = $http({
+        method: "GET",
+        url: (ApiBasePath + "/menu_items.json")
+        });
 
         return response.then(function (result) {
+            // process result and only keep items that match
             var foundItems = [];
             var menuItems = result.data.menu_items;
             for (var i = 0; i < menuItems.length; i++) {
-                var description = menuItems[i].description.toLowerCase();
-                if (description.indexOf(searchTerm.toLowerCase()) !== -1) {
-                    foundItems.push(menuItems[i]);
-                }
+            var description = menuItems[i].description.toLowerCase();
+            if (description.indexOf(searchTerm.toLowerCase()) !== -1) {
+                foundItems.push(menuItems[i]);
             }
+            }
+
+            // return processed items
             return foundItems;
         });
     };
-}
+    }
     
 
     function foundItemsDirective() {
