e7bbc6fa1af8538f6c239159a7e9c276ed779125
Sergey Lazarenko <rikitikitavi69@gmail.com>
2019-09-22 21:29:55 +0300

    fixes

--- solution-4/src/menudata.service.js
+++ solution-4/src/menudata.service.js
@@ -12,6 +12,8 @@
             return $http({
                 method: 'GET',
                 url: 'https://davids-restaurant.herokuapp.com/categories.json'
+            }).then(function(response) {
+                return response.data;
             });
         }
 
@@ -20,6 +22,8 @@
                 method: 'GET',
                 url: 'https://davids-restaurant.herokuapp.com/menu_items.json',
                 params: {category: categoryShortName}
+            }).then(function(response) {
+                return response.data;
             });
         }
     }
--- solution-4/src/routes.js
+++ solution-4/src/routes.js
@@ -8,8 +8,8 @@
     RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
     function RoutesConfig($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/');
+        
         $stateProvider
-
         .state('home', {
             url: '/',
             tempalateUrl: 'src/templates/home.template.html'
@@ -21,9 +21,7 @@
             controller: 'CategoriesController as categoriesCtrl',
             resolve: {
                 categories: ['MenuDataService', function(MenuDataService) {
-                    return MenuDataService.getAllCategories().then(function(response) {
-                        return response.data;
-                    });
+                    return MenuDataService.getAllCategories();
                 }]
             }
         })
@@ -34,9 +32,7 @@
             controller: 'ItemsController as itemsCtrl',
             resolve: {
                 items: ['MenuDataService', function(MenuDataService) {
-                    return MenuDataService.getItemsForCategory($stateParams.category).then(function(response) {
-                        return response.data.menu_items;
-                    })
+                    return MenuDataService.getItemsForCategory($stateParams.category)
                 }]
             }
         })
