fdaa15dca6cb9a8dda3f36d89faae17ed6ffb907
Sergey Lazarenko <rikitikitavi69@gmail.com>
2019-09-22 16:52:08 +0300

    upd

--- solution-3/js/app.js
+++ solution-3/js/app.js
@@ -1,57 +1,36 @@
-(function(){
-    "use strict";
-    
+(function () {
+    'use strict';
+
     angular.module('NarrowItDownApp', [])
-    .controller('NarrowItDownController', NarrowItDownController)
-    .service('MenuSearchService', MenuSearchService)
-    .directive('foundItems', FoundItemsDirective)
-    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItemsDirective)
+        .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com');
 
-    
-    function FoundItemsDirective() {
-        var ddo = {
-            templateUrl: 'itemsloader.html',
-            scope: {
-                found: '<',
-                onRemove: '&'
-            },
-            controller: FoundItemsDirectiveController,
-            controllerAs: 'list',
-            bindToController: true
-        };
-        return ddo;
+
+    NarrowItDownController.$inject = ['MenuSearchService']
+    function NarrowItDownController(MenuSearchService) {
+        var narrow = this;
+        var check = false;
+        narrow.searchTerm = "";
+        narrow.found = []
+        narrow.updatefound = function () {
+            check = true;
+
+            var promise = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
+            promise.then(function (result) {
+                narrow.found = result;
+            }).catch(function (error) { console.log(error); })
+
+        }
+        narrow.removeItem = function (itemIndex) {
+            MenuSearchService.removeItem(itemIndex);
+
+        }
+        narrow.noItemsFound = function () {
+            return (narrow.found.length == 0 || narrow.searchTerm === "") && check;
+        }
     }
-
-    function FoundItemsDirectiveController() {
-        var list = this;
-      }
-
-
-      NarrowItDownController.$inject = ['MenuSearchService']
-      function NarrowItDownController(MenuSearchService) {
-          var narrow = this;
-          var check = false;
-          narrow.searchTerm = "";
-          narrow.found = []
-          narrow.updatefound = function () {
-              check = true;
-  
-              var promise = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
-              promise.then(function (result) {
-                  narrow.found = result;
-              }).catch(function (error) { console.log(error); })
-  
-          }
-          narrow.removeItem = function (itemIndex) {
-              MenuSearchService.removeItem(itemIndex);
-  
-          }
-          narrow.noItemsFound = function () {
-              return (narrow.found.length == 0 || narrow.searchTerm === "") && check;
-          }
-      }
-  
-
 
     MenuSearchService.$inject = ['$http', 'ApiBasePath'];
     function MenuSearchService($http, ApiBasePath) {
@@ -71,6 +50,7 @@
                 for (var i = 0; i < result.data.menu_items.length; i++) {
 
                     var description = result.data.menu_items[i].description;
+
                     if (description.toLowerCase().indexOf(searchTerm.toLowerCase()) != -1) {
                         foundItems.push(result.data.menu_items[i]);
                     }
@@ -83,5 +63,25 @@
             foundItems.splice(itemIndex, 1);
         };
     }
-    
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            scope: {
+                items: '<',
+                onRemove: '&'
+            },
+            controller: FoundItemsDirectiveController,
+            controllerAs: 'list',
+            bindToController: true,
+        };
+
+        return ddo;
+
+    }
+    function FoundItemsDirectiveController() {
+        var list = this;
+
+    }
+
+
 })();
